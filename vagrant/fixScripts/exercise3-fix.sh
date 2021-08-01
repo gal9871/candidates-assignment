bfd4314f980876953ee3ddd216161655a8268d2c
Gal Segal <gal9871@gmail.com>
2021-08-02 00:54:10 +0300

    finished assignment

--- vagrant/fixScripts/exercise3-fix.sh
+++ vagrant/fixScripts/exercise3-fix.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 #add fix to exercise3 here
+sudo sed -i 's/Require all denied/Require all granted/g' /etc/apache2/sites-enabled/000-default.conf ; sudo systemctl restart apache2.service
--- vagrant/fixScripts/exercise5-fix_server1.sh
+++ vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 #add fix to exercise5-server1 here
+ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa ;  sshpass -p vagrant ssh-copy-id -i ~/.ssh/id_rsa.pub server2 ; ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o PasswordAuthentication=no server2
